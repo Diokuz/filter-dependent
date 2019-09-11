a1bc2b834d681a9dc94d94ba899e8c0d7002e8ad
ilrilan <ilrilan2@gmail.com>
2019-09-11 20:07:47 +0500

    Исправлена работа утилиты под OS Windows

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
-const pathToUnixPath = (str: string) => str.replace(/\\/g, '/')
+const os = require('os')
+
+const convertWinPath = (str: string) => str.replace(/\\/g, '/')
+const pathToUnixPath = os.platform() === 'win32' ? convertWinPath : (str: string) => str
 
 export { pathToUnixPath }
